5bc8de9358facc1a7fb09d0ea15e18cfd5a6893a
ariddell <ariddell>
2017-07-04 21:53:56 -0400

    add required config.sh

---
+++ config.sh
@@ -0,0 +1,24 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function pip_opts {
+    # Extra options for pip
+    if [ -n "$IS_OSX" ]; then
+        local suffix=scipy_installers
+    else
+        local suffix=manylinux
+    fi
+    echo "--only-binary matplotlib --find-links https://nipy.bic.berkeley.edu/$suffix"
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python -m pytest tests
+}
