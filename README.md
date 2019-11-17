e71144cd24b3d6768cc19811d00f14554f03e231
Allen Riddell <riddella+stan@indiana.edu>
2019-11-17 18:44:57 -0500

    Initial commit, httpstan v1.0.0

---
+++ README.md
@@ -0,0 +1,16 @@
+httpstan-wheels
+===============
+
+See https://travis-ci.org/MacPython/matplotlib-wheels for details on how this
+all works.
+
+
+## Notes
+
+*Differences from the standard ``multibuild`` instructions are noted in this section.*
+
+Stan version 2.19 and higher requires C++14. In ``env_vars.sh`` we ``export MACOSX_DEPLOYMENT_TARGET=10.9``
+so that ``clang`` will use ``libc++`` for the C++ standard library.
+
+We also use a custom `xenial` image because to even test pystan we need a
+version of gcc more recent than the one that ships with trusty.
---
+++ config.sh
@@ -0,0 +1,24 @@
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    if [ -n "$IS_OSX" ]; then
+        export CC=clang
+        export CXX=clang++
+    fi
+    echo "in pre_build"
+    SRC_DIR=httpstan
+    pip install -r $SRC_DIR/requirements.txt
+    pip install grpcio-tools
+    # FIXME: does pre_build get executed twice? make reports "nothing to do"
+    make -C $SRC_DIR
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python -c 'import httpstan'
+    # empty directory is inside the repo directory (see ``install_run``)
+    SRC_DIR=httpstan
+    pip install -r ../$SRC_DIR/test-requirements.txt
+    python -m pytest ../$SRC_DIR/tests
+}
