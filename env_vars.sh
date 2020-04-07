c05f87aa42d5125810dd8c8d9241d95ba7458aa9
Allen Riddell <riddella+stan@indiana.edu>
2020-04-07 12:03:31 -0400

    Use manylinux2014, update multibuild

--- env_vars.sh
+++ env_vars.sh
@@ -1,6 +1,2 @@
 # See multibuild/README.rst
 export MACOSX_DEPLOYMENT_TARGET=10.9
-export CFLAGS="-static-libstdc++"
-export CC=/usr/local/gcc-8.3.0/bin/gcc-8.3.0
-export CXX=/usr/local/gcc-8.3.0/bin/g++-8.3.0
-export LD_LIBRARY_PATH="/usr/local/gcc-8.3.0/lib64/:${LD_LIBRARY_PATH}"
